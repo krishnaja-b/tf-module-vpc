59c1542e5edc664e9a01fbe599514f743500cf57
krish <krishnaja-b>
2023-08-03 13:32:48 +0530

    adding peering in public n private route table

--- main.tf
+++ main.tf
@@ -5,6 +5,17 @@ resource "aws_vpc" "main" {
     { Name = "${var.env}-vpc" }
   )
 }
+# peering connection
+resource "aws_vpc_peering_connection" "peer" {
+  peer_owner_id = data.aws_caller_identity.account.account_id
+  peer_vpc_id   = var.default_vpc_id
+  vpc_id        = aws_vpc.main.id
+  auto_accept = true
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-peer" }
+  )
+}
 
 # public subnets
 resource "aws_subnet" "public_subnets" {
@@ -32,6 +43,12 @@ route {
   cidr_block = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
+# adding peering in public route table
+route {
+  cidr_block = data.aws_vpc.default_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
 
 # internet gateway
 
@@ -90,6 +107,18 @@ resource "aws_route_table" "private-route-table" {
   )
   for_each = var.private_subnets
 }
+# attach natgateway to private route table
+
+route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+# adding peering in private route table
+route {
+  cidr_block = data.aws_vpc.default_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
+
 #associate route table
 resource "aws_route_table_association" "private-association" {
   for_each = var.private_subnets
