6861473e969a67f90380a64e6d5f5cdb9b0c58b4
krish <krishnaja-b>
2023-08-10 11:04:14 +0530

    to get only privata subnet id's in docdb cluster

--- main.tf
+++ main.tf
@@ -125,7 +125,13 @@ resource "aws_route_table_association" "private-association" {
   subnet_id      =lookup(lookup(aws_subnet.private_subnets,each.value["name"],null),"id",null)
   route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
+# adding peering in the default route table
 
+resource "aws_route" "route" {
+  route_table_id = var.default_route_table
+  destination_cidr_block = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 
 
 
